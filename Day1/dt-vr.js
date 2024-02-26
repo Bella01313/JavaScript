9fe772868b8658efefcfa6fe5dd2db32d17e7ffe
Bella01313 <ingabire.bella.muhizi@gmail.com>
2024-02-26 13:09:53 +0200

    first commit

---
+++ Day1/dt-vr.js
@@ -0,0 +1,13 @@
+const age =18;
+const myName = "Bella";
+const isSheagrirl =true;
+const array = ["string", 12 , false]
+console.log("My name is " +myName+ " and am " +age+ " years old");
+console.log(`My name is ${myName} and am ${age} years old`);
+console.log(array);
+
+function myLastName (){
+    let me = "Bella";
+    console.log(me);
+}
+myLastName();
